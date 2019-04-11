30c96b7739573458b2fedbbab26d160c2f421342
agitoz <rteja764@gmail.com>
2019-04-11 16:11:53 +0200

    100 iterations

--- rawData_to_diffused.sh
+++ rawData_to_diffused.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-time python ../Weighted_Graph_Python_GPU/graph_gpu.py -p ./data/position_image.npz -t ./data/texture_image.npz -k 4 -s 1000000000
+cd ../Weighted_Graph_Python_GPU/
+time python graph_gpu.py -p ../Python_Graph_Heat_Diffusion_Gpu/data/position_image.npz -t ../Python_Graph_Heat_Diffusion_Gpu/data/texture_image.npz -k 4 -s 1000000000
+cd -
 cp ../Weighted_Graph_Python_GPU/graphs/graph_gpu.npz ./data/
 time python main.py -i ./data/graph_gpu.npz -t ./data/texture_image.npz -k 4 -it 100 
